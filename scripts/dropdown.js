a49c97176bb422c37b1818a152d07a1b913cf26f
ramses <ramses.romero.jr@gmail.com>
2020-11-04 18:43:03 -0500

    search

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,12 +1,22 @@
 window.addEventListener('DOMContentLoaded', ()=>{
     let config = document.querySelector('.fa-cog');
-    
+    let searchBox = document.querySelector('.searchBox')
+
     config.addEventListener('click',(e)=>{
         let dropdown = document.querySelector('.pref')
         if (dropdown.classList.contains('pref--hidden')) {
             dropdown.classList.remove('pref--hidden')
         } else {
             dropdown.classList.add('pref--hidden')
+        }
+    })
+
+    document.querySelector(".fa-search")
+    .addEventListener('click', ()=>{
+        if (searchBox.classList.contains('search--hidden')) {
+            searchBox.classList.remove('search--hidden')
+        } else {
+            searchBox.classList.add('search--hidden')
         }
     })
 })
